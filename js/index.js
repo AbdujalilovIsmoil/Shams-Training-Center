7fc9affb6539f2632c08929b3e489f380d4ddd56
Coders Uz <ismoilbek20050421@gmail.com>
2025-03-04 16:07:33 +0500

    Added javascript codes for testimonials

--- js/index.js
+++ js/index.js
@@ -16,10 +16,94 @@ const init = () => {
     return element;
   };
 
+  const testimonials = [
+    {
+      id: 1,
+      title: "Abdulloh M.",
+      text: `
+        Avval ko‘p joylarda arab tili o‘rganganman, lekin bu
+        markazning uslubi juda zo‘r! Juda qisqa vaqt ichida CEFR
+        imtihoniga tayyor bo‘ldim va natijam ham juda yaxshi bo‘ldi.
+        Eng yaxshi o‘quv markazlaridan biri!
+      `,
+    },
+    {
+      id: 2,
+      title: "Xadicha S.",
+      text: `
+        Men Shams o‘quv markazida 3 oy o‘qib, arab tilida gapirishni
+        o‘rgandim. Darslar juda qiziqarli va samarali. Eng yaxshi
+        tomoni – darslarni qayta ko‘rib chiqish imkoniyati borligi.
+        Chunki bu bilan bilimni yanada mustaxkamlash mumkin!
+      `,
+    },
+    {
+      id: 3,
+      title: "Hasanbek T.",
+      text: `
+        Oldin arab tilini tushunish menga juda qiyin tuyulardi, lekin
+        Shams markazi darslari orqali qisqa vaqt ichida so‘z boyligim
+        oshdi va grammatikani tushuna boshladim. Endi ishonch bilan
+        gapira olaman!
+      `,
+    },
+    {
+      id: 4,
+      title: "Zaynab A.",
+      text: `
+        O‘qitish jarayoni tushunarli va samarali. Qo‘shimcha
+        materiallar beriladi va bu darslarni yanada foydali qiladi,
+        shuning uchun har safar ulardan foydalanib bo’lgandan so’ng
+        yangi darsni kutaman!
+      `,
+    },
+    {
+      id: 5,
+      title: "Abdulloh M.",
+      text: `
+        Avval ko‘p joylarda arab tili o‘rganganman, lekin bu
+        markazning uslubi juda zo‘r! Juda qisqa vaqt ichida CEFR
+        imtihoniga tayyor bo‘ldim va natijam ham juda yaxshi bo‘ldi.
+        Eng yaxshi o‘quv markazlaridan biri!
+      `,
+    },
+    {
+      id: 6,
+      title: "Xadicha S.",
+      text: `
+        Men Shams o‘quv markazida 3 oy o‘qib, arab tilida gapirishni
+        o‘rgandim. Darslar juda qiziqarli va samarali. Eng yaxshi
+        tomoni – darslarni qayta ko‘rib chiqish imkoniyati borligi.
+        Chunki bu bilan bilimni yanada mustaxkamlash mumkin!
+      `,
+    },
+    {
+      id: 7,
+      title: "Hasanbek T.",
+      text: `
+        Oldin arab tilini tushunish menga juda qiyin tuyulardi, lekin
+        Shams markazi darslari orqali qisqa vaqt ichida so‘z boyligim
+        oshdi va grammatikani tushuna boshladim. Endi ishonch bilan
+        gapira olaman!
+      `,
+    },
+    {
+      id: 8,
+      title: "Zaynab A.",
+      text: `
+        O‘qitish jarayoni tushunarli va samarali. Qo‘shimcha
+        materiallar beriladi va bu darslarni yanada foydali qiladi,
+        shuning uchun har safar ulardan foydalanib bo’lgandan so’ng
+        yangi darsni kutaman!
+      `,
+    },
+  ];
+
   const questionsBox = document.querySelectorAll(".questions__box");
   const questionsItem = document.querySelectorAll(".questions__item");
-  const questionsBoxIcon = document.querySelectorAll(".questions__box-icon");
+  const testimonialsList = document.querySelector(".testimonials__list");
   const questionsContent = document.querySelectorAll(".questions__content");
+  const questionsBoxIcon = document.querySelectorAll(".questions__box-icon");
 
   const hideElements = () => {
     for (let i = 0; i < questionsItem.length; i++) {
@@ -53,6 +137,21 @@ const init = () => {
     };
 
     questionsBox[i].addEventListener("click", openContent);
+  }
+
+  for (let i = 0; i < testimonials.length; i++) {
+    const element = createElement(
+      "li",
+      "testimonials__item swiper-slide",
+      `
+       <h2 class="testimonials__item-title">${testimonials[i].title}</h2>
+          <p class="testimonials__item-text">
+            <q>${testimonials[i].text}</q>
+          </p>
+      `
+    );
+
+    testimonialsList.append(element);
   }
 
   const swiper = new Swiper(".swiper", {
