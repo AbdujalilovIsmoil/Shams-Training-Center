5881fac8bc7bb08e7b1b779c2f9514aa989a2ef3
Coders Uz <ismoilbek20050421@gmail.com>
2025-03-20 12:04:34 +0500

    Added select option languages

--- js/index.js
+++ js/index.js
@@ -262,8 +262,6 @@ Foydalanuvchining raqami: ${userNumber.value}
 
   const text = document.querySelector(".hero__play-text");
 
-  console.log();
-
   const newText = text.innerText
     .split("")
     .map((char, i) => {
@@ -272,6 +270,43 @@ Foydalanuvchining raqami: ${userNumber.value}
     .join("");
 
   text.innerHTML = newText;
+
+  const chevronDown = document.querySelector(".fa-chevron-down");
+  const selectTop = document.querySelector(".header-select__top");
+  const icons = document.querySelectorAll(".header-select__language");
+  const headerTopText = document.querySelector(".header-select__top-text");
+  const selectLanguages = document.querySelector(".header-select-languages");
+
+  const openLanguages = () => {
+    chevronDown.classList.toggle("active");
+    selectLanguages.classList.toggle("header-select-languages--open");
+  };
+
+  selectTop.addEventListener("click", openLanguages);
+
+  const hideIcon = () => {
+    icons.forEach((el) => {
+      el.children[1].classList.remove("visible");
+    });
+  };
+
+  const showIcon = (i) => {
+    icons[i].children[1].classList.add("visible");
+
+    headerTopText.innerHTML = icons[i].children[0].innerHTML;
+  };
+
+  hideIcon();
+  showIcon(0);
+
+  icons.forEach((el, i) => {
+    el.addEventListener("click", (e) => {
+      hideIcon();
+      showIcon(i);
+
+      openLanguages();
+    });
+  });
 };
 
 window.addEventListener("DOMContentLoaded", init);
