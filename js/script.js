4d3627d424bc6f07f50f75e214bc89d048c3d7ac
Coders Uz <ismoilbek20050421@gmail.com>
2025-03-31 10:00:43 +0500

    Added header javascript codes

--- js/script.js
+++ js/script.js
@@ -4,19 +4,28 @@ const faBars = document.querySelector(".fa-bars");
 const navbarLinks = document.querySelectorAll(".navbar__item-link");
 const headerContactBtn = document.querySelector(".header-contact-btn");
 
+const closeNavbar = () => {
+  navbar.classList.remove("navbar--open");
+  headerContactBtn.children[0].setAttribute("class", "fa fa-bars");
+};
+
 headerContactBtn.addEventListener("click", function () {
   if (this.children[0].getAttribute("class") === "fa fa-bars") {
     navbar.classList.add("navbar--open");
     this.children[0].setAttribute("class", "fa fa-times");
   } else {
-    navbar.classList.remove("navbar--open");
-    this.children[0].setAttribute("class", "fa fa-bars");
+    closeNavbar();
   }
 });
 
 for (const navbarLink of navbarLinks) {
   navbarLink.addEventListener("click", () => {
-    navbar.classList.remove("navbar--open");
-    headerContactBtn.children[0].setAttribute("class", "fa fa-bars");
+    closeNavbar();
   });
 }
+
+navbar.addEventListener("click", (e) => {
+  if (e.target.classList.contains("navbar--open")) {
+    closeNavbar();
+  }
+});
